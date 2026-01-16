ae6d9037dcfbfca78dec08e5430ab18344d4178e
RicardoBS04 <j.bolanos.2@estudiantec.cr>
2026-01-15 19:11:26 -0600

    Agregué una función a cliente

--- Cliente/Program.cs
+++ Cliente/Program.cs
@@ -1,34 +1,43 @@
 ﻿using System;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 using System.Text;
 
-class Program
+
+namespace cliente
 {
-    static void Main()
+    class Program
     {
-        string ipServidor = "127.0.0.1";
-        int puerto = 5000;
+        static void Main()
+        {
+            string ipServidor = "127.0.0.1";
+            int puerto = 5000;
 
-        TcpClient cliente = new TcpClient();
+            TcpClient cliente = new TcpClient();
 
-        cliente.Connect(ipServidor, puerto);
-        Console.WriteLine("Conectado al servidor");
+            cliente.Connect(ipServidor, puerto);
+            Console.WriteLine("Conectado al servidor");
 
-        NetworkStream stream = cliente.GetStream();
+            NetworkStream stream = cliente.GetStream();
 
-        Console.Write("Ingrese una expresión: ");
-        string mensaje = Console.ReadLine();
+            byte[] buffer = new byte[1024];
+            int bytesLeidos = stream.Read(buffer, 0, buffer.Length);
+            string respuesta = Encoding.UTF8.GetString(buffer, 0, bytesLeidos);
 
-        byte[] datos = Encoding.UTF8.GetBytes(mensaje);
-        stream.Write(datos, 0, datos.Length);
+            Console.WriteLine("Respuesta del servidor: " + respuesta);
 
-        byte[] buffer = new byte[1024];
-        int bytesLeidos = stream.Read(buffer, 0, buffer.Length);
-        string respuesta = Encoding.UTF8.GetString(buffer, 0, bytesLeidos);
+            stream.Close();
+            cliente.Close();
+        }
 
-        Console.WriteLine("Respuesta del servidor: " + respuesta);
-
-        stream.Close();
-        cliente.Close();
+        public void enviar_mensaje(string dato)
+        {
+            byte[] datos = Encoding.UTF8.GetBytes(dato);
+            stream.Write(datos, 0, datos.Length);
+            Console.WriteLine("Mensaje enviado con éxito");
+        }
     }
 }
+
